988853a54d7e584d950e3fee2c1e8a7f80da5ea5
makka-kalyan <kalyanmakka27@gmail.com>
2026-06-22 10:29:46 +0530

    Add user registration and login pages

---
+++ 1.DonationCenter-Frontend/src/Components/Login.jsx
@@ -0,0 +1,109 @@
+import "./Login.css";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const [message, setMessage] = useState("");
+  const [msgType, setMsgType] = useState("");
+
+  const [data, setData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post(
+        "http://127.0.0.1:8000/api/login/",
+        data
+      );
+
+      if (res.data.error === "Account not approved yet") {
+        setMsgType("error");
+        setMessage("Awaiting Admin approval");
+        return;
+      }
+
+      if (res.data.role === "admin") {
+        localStorage.setItem("isUserLoggedIn", "true");
+        localStorage.setItem("username", res.data.username);
+        localStorage.setItem("role", "admin");
+        navigate("/admin");
+        return;
+      }
+
+      if (res.data.role === "user") {
+        localStorage.setItem("isUserLoggedIn", "true");
+        localStorage.setItem("username", res.data.username);
+        localStorage.setItem("role", "user");
+        navigate("/user");
+        return;
+      }
+
+      setMsgType("error");
+      setMessage("Invalid Credentials");
+
+    } catch (err) {
+      setMsgType("error");
+      setMessage("Invalid username or password");
+    }
+  };
+
+  return (
+    <>
+      <main>
+        <h2>Login</h2>
+
+        {message && (
+          <p className={`msg ${msgType}`} style={{ textAlign: "center" }}>
+            {message}
+          </p>
+        )}
+
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Name:</label>
+            <input
+              name="username"
+              type="text"
+              autoFocus
+              placeholder="Enter Username"
+              onChange={(e) =>
+                setData({ ...data, username: e.target.value })
+              }
+              required
+            />
+          </div>
+
+          <div>
+            <label>Password:</label>
+            <input
+              name="password"
+              placeholder="Enter Password"
+              type="password"
+              onChange={(e) =>
+                setData({ ...data, password: e.target.value })
+              }
+              required
+            />
+          </div>
+
+          <button type="submit" id="log-btn">
+            Submit
+          </button>
+        </form>
+      </main>
+
+      <footer>
+        <h4>&copy; 2025 All Rights Reserved SAK Informatics</h4>
+      </footer>
+    </>
+  );
+};
+
+export default Login;
---
+++ 1.DonationCenter-Frontend/src/Components/Register.jsx
@@ -0,0 +1,100 @@
+import "./Register.css"
+import axios from "axios"
+import { useState } from "react"
+
+const Register=()=>{
+  const [message, setMessage] = useState("");
+  const [msgType, setMsgType] = useState("");
+
+      const [form, setForm] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirm_password: "",
+    mobile: "",
+    address: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await axios.post(
+      "http://127.0.0.1:8000/api/register/",
+      form
+    );
+     if(res.data.error){
+  setMsgType("error");
+  setMessage(res.data.error);
+    }else{
+      setMsgType("success");
+      setMessage(res.data.success);
+    }
+
+    
+  };
+
+    return(<>
+      <main>
+        <h2>Register</h2>
+      {message && (
+        <p className={`msg ${msgType}`}>
+          {message}
+        </p>
+      )}
+
+     <form onSubmit={handleSubmit}>
+         <div>
+            <label>Name:</label>
+            <input type="text" name="username" onChange={handleChange} placeholder="Enter User Name" required autoFocus/>
+        </div>
+
+        <div>
+            <label>Email:</label>
+            <input type="email" name="email" onChange={handleChange} placeholder="Enter Email" required/>
+        </div>
+
+        <div>
+            <label>Password:</label>
+            <input type="password" name="password" onChange={handleChange} placeholder="Enter Password" required/>
+        </div>
+
+        <div>
+            <label>Confirm Password:</label>
+            <input type="password" name="confirm_password" onChange={handleChange} placeholder="Re-enter password" required/>
+        </div>
+
+        <div>
+            <label>Mobile Number:</label>
+            <input 
+                type="tel"
+                name="mobile"
+                inputMode="numeric" 
+                  maxLength="10"
+                pattern="[0-9]{10}"
+                title="Please enter exactly 10 digits"
+                placeholder="Enter mobile number"
+                onChange={handleChange}
+                required
+              />
+        </div>
+
+        <div>
+            <label>Address:</label>
+            <textarea name="address" onChange={handleChange} rows="2" cols="30" placeholder="Enter full address" required></textarea>
+        </div>
+
+        <button type="submit" id="btn-sub">Submit</button>
+
+    </form>
+      </main>
+      
+       <footer>
+             <h4>&copy; 2025 All Rights Reserved SAK Informatics</h4>
+        </footer>
+    </>)
+}
+export default Register
