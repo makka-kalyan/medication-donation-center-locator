ab4a44430816c1e5962ab42c82ae2835f9de6a96
makka-kalyan <kalyanmakka27@gmail.com>
2026-06-23 12:23:52 +0530

    Restore home page background design

--- 1.DonationCenter-Frontend/src/Components/Home.jsx
+++ 1.DonationCenter-Frontend/src/Components/Home.jsx
@@ -1,20 +1,29 @@
-import "./Home.css"
+import "./Home.css";
 
-const Home=()=>{
-    return(<>
-        <main>
-            <h2 id="home-head">Welcome To Home Page</h2>
-            <div id="para-div1">
-               <p>The Medication Donation Center Locator is a health-focused web application designed to help people safely donate
-                 unused or extra medicines to authorized donation centers. The system ensures that medicines reach those in need 
-                 while following proper safety and compliance rules.</p>  
-                <p>The application includes an admin-controlled approval system, where users can register but gain access only
-                     after admin verification. This prevents misuse and ensures platform authenticity.</p>
-            </div>
-        </main>
-        <footer>
-             <h4>&copy; 2025 All Rights Reserved SAK Informatics</h4>
-        </footer>
-    </>)
-}
-export default Home
+const Home = () => {
+  return (
+    <>
+      <main className="home-page">
+        <h2 id="home-head">Welcome To Home Page</h2>
+        <div id="para-div1">
+          <p>
+            The Medication Donation Center Locator is a health-focused web
+            application designed to help people safely donate unused or extra
+            medicines to authorized donation centers. The system ensures that
+            medicines reach those in need while following proper safety and
+            compliance rules.
+          </p>
+          <p>
+            The application includes an admin-controlled approval system, where
+            users can register but gain access only after admin verification.
+            This prevents misuse and ensures platform authenticity.
+          </p>
+        </div>
+      </main>
+      <footer>
+        <h4>&copy; 2025 All Rights Reserved SAK Informatics</h4>
+      </footer>
+    </>
+  );
+};
+export default Home;
